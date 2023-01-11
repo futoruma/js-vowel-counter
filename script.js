1d999bcf81354a4cd267d9a3b830701f4f571820
futoruma <evgeny.martov@gmail.com>
2023-01-11 17:00:48 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const textAreaInput = document.querySelector("#textinput")
+const submitButton = document.querySelector("#submit")
+
+submitButton.addEventListener("click", () => {
+    let newText = textAreaInput.value.toLowerCase()
+
+    let vowels_count = 0
+    for (let i = 0; i < newText.length; i++) {
+        if (newText[i] == "a" ||
+            newText[i] == "i" ||
+            newText[i] == "u" ||
+            newText[i] == "e" ||
+            newText[i] == "o") {
+            vowels_count++
+        }
+    }
+
+    alert(`Vowels: ${vowels_count}`)
+})
